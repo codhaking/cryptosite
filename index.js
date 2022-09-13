f07f8f8b6819f4e7d55c4c05ab5fffa45b41f7da
Amichania <onwubikookem@gmail.com>
2022-09-13 09:45:05 +0100

    added js files

---
+++ index.js
@@ -0,0 +1,17 @@
+const clickthis = document.getElementById('card1');
+
+clickthis.addEventListener('click', function onClick(event){
+	event.target.style.backgroundColor = '#2b076e';
+	})
+
+const doubleclick = document.getElementById('card2');
+
+doubleclick.addEventListener('click', function onClick(event){
+	event.target.style.backgroundColor = '#2b076e';
+	})
+
+const tripleclick = document.getElementById('card3');
+
+tripleclick.addEventListener('click', function onClick(event){
+	event.target.style.backgroundColor = '#2b076e';
+	})
